f0d26b3053d4315634942ee0d13745aca2667def
Mingyi ZHANG <mingyizhang1987@gmail.com>
2017-07-28 07:09:22 +0200

    Modify project translator, version 2

--- projects/language-translation/command.sh
+++ projects/language-translation/command.sh
@@ -1,3 +1,3 @@
 cp -r /code/* /output
 cd /output
-/run_jupyter.sh --no-browser --NotebookApp.base_url='/notebooks/qmY64wgaNbXKipu8LCeBAD' --NotebookApp.token='' --NotebookApp.allow_origin='*'
+/run_jupyter.sh --no-browser --NotebookApp.base_url='/notebooks/z2Wq5HToBZ3hg53ganCMQi' --NotebookApp.token='' --NotebookApp.allow_origin='*'
